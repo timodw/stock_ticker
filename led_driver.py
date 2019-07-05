aa24087682fe312bb32d47c1449f7e44cd4ab3c8
Timo De Waele <timo.dewaele@gmail.com>
2019-07-05 18:09:23 +0000

    added led driver

---
+++ led_driver.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+
+class LEDDriver:
+
+    def __init__(self, green_pin_nr, red_pin_nr):
+        self.green_led = green_pin_nr
+        self.red_led = red_pin_nr
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.green_led, GPIO.OUT)
+        GPIO.setup(self.red_led, GPIO.OUT)
+        GPIO.output(self.red_led, GPIO.HIGH)
+        self.red_on = True
+
+    def toggle(self):
+        if self.red_on:
+            GPIO.output(self.red_led, GPIO.LOW)
+            GPIO.output(self.green_led, GPIO.HIGH)
+            self.red_on = False
+        else:
+            GPIO.output(self.green_led, GPIO.LOW)
+            GPIO.output(self.red_led, GPIO.HIGH)
+            self.red_on = True
+
+
+if __name__ == "__main__":
+    led_driver = LEDDriver(25, 24)
+    while True:
+        led_driver.toggle()
+        time.sleep(1)
